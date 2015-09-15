76465016eef2075d2d7b678c3e4082c1fe24804b
dcb9 <jt1dcb@163.com>
2015-09-15 15:35:54 +0800

    fix enviroments

--- environments/index.php
+++ environments/index.php
@@ -29,8 +29,6 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
             'frontend/runtime',
             'frontend/web/assets',
         ],
@@ -38,15 +36,12 @@ return [
             'yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
             'frontend/config/main-local.php',
         ],
     ],
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
             'frontend/runtime',
             'frontend/web/assets',
         ],
@@ -54,7 +49,6 @@ return [
             'yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
             'frontend/config/main-local.php',
         ],
     ],
